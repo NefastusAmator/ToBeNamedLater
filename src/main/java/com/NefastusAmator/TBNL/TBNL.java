074a51d36259f9a737ff939e44b28b6239d3822d
NefastusAmator <nefastusamator@gmail.com>
2015-01-28 14:29:32 -0500

    Testing

---
+++ src/main/java/com/NefastusAmator/TBNL/TBNL.java
@@ -0,0 +1,29 @@
+package com.NefastusAmator.TBNL;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="TBNL", name="To Be Named Later", version = "1.7.10-1.0")
+public class TBNL
+{
+    @Mod.Instance("TBNL")
+    public static TBNL instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
